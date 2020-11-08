469ddfcb4a46c959e6130b03a4ba03a5a4ca62a1
ThrugansDDOS <74088979+ThrugansDDOS@users.noreply.github.com>
2020-11-08 18:49:20 +0700

    Add files via upload

---
+++ Tools-T•DosV1.sh
@@ -0,0 +1,97 @@
+#Tools Milik Thrugans-DDOS
+#Jangan Di Salah Gunakan
+#Jangan Di Record Anjing
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay Welcome To
+toilet -f big -F gay Tools Thrugans
+sleep 1
+echo
+echo $green"Tools Ini Dibuat Oleh Thrugans-DDOS"
+sleep 1
+echo $white"Jangan Di Salah Gunakan Ok"
+sleep 1
+echo $green"Kami Hacker Yang Bersatu"
+sleep 1
+echo $white"Kami Ada Banyak Tapi Tidak Terlihat"
+sleep 2
+echo
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+toilet -f big -F gay Pilih Tools
+sleep 2
+echo
+echo $yellow"╔════════════════════════"
+echo $yellow"╠➣[1]•Hack Facebook"
+sleep 0.5
+echo $yellow"╠➣[2]•Hack Pulsa Operator"
+sleep 0.5
+echo $yellow"╠➣[3]•Hack Tools Bokep"
+echo $yellow"╚════════════════════════"
+sleep 0.5
+echo
+read -p "Pilih: " pil
+#batas
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+toilet -f big -F gay Loading
+sleep 1
+echo
+echo $white"Sabar Orang Sabar Di Sayang Janda"
+sleep 3
+echo $white"Yang Sabar Ya Tolol Susah Buatnya"
+sleep 5
+echo
+clear
+git clone https://github.com/pirmansx/mbf
+cd mbf
+pip install mechanize
+python2 MBF.py
+fi
+#batas
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+toilet -f big -F gay Loading
+sleep 1
+echo
+echo $white"Sabar Orang Sabar Di Sayang Janda"
+sleep 3
+echo $white"Yang Sabar Ya Tolol Susah Buatnya"
+sleep 5
+echo
+clear
+git clone https://github.com/novalattasya/darkpulsa
+cd darkpulsa
+sh Pulsa.sh
+fi
+#batas
+if [ $pil = 3 ] || [ $pil = 3 ]
+then
+clear
+toilet -f big -F gay Loading
+sleep 1
+echo
+echo $white"Sabar Orang Sabar Di Sayang Janda"
+sleep 3
+echo $white"Yang Sabar Ya Tolol Susah Buatnya"
+sleep 5
+echo
+clear
+git clone https://github.com/TumbalGanss/ToolsBkp
+cd ToolsBkp
+sh Tobat.sh
+fi
